33ce813faa7707db8536b541660237a3ac97f3b4
Timur Asaliev <t.asaliev@qobo.biz>
2019-01-17 14:53:16 +0200

    Improve test bootstrap (task #9585)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 use Cake\Core\Configure;
 use Cake\Filesystem\Folder;
 
-$pluginName = 'Qobo\Social';
+$pluginName = 'Social';
 if (empty($pluginName)) {
     throw new \RuntimeException("Plugin name is not configured");
 }
@@ -46,7 +46,7 @@ require ROOT . '/vendor/autoload.php';
 require CORE_PATH . 'config/bootstrap.php';
 
 Configure::write('App', [
-    'namespace' => $pluginName . '\Test\App',
+    'namespace' => 'Qobo\\' . $pluginName . '\Test\App',
     'paths' => [
         'templates' => [
             APP . 'Template' . DS
@@ -103,8 +103,8 @@ Cake\Datasource\ConnectionManager::setConfig('test', [
 ]);
 
 // Alias AppController to the test App
-class_alias($pluginName . '\Test\App\Controller\AppController', 'App\Controller\AppController');
+class_alias('Qobo\\' . $pluginName . '\Test\App\Controller\AppController', 'App\Controller\AppController');
 // If plugin has routes.php/bootstrap.php then load them, otherwise don't.
 $loadPluginRoutes = file_exists(ROOT . DS . 'config' . DS . 'routes.php');
 $loadPluginBootstrap = file_exists(ROOT . DS . 'config' . DS . 'bootstrap.php');
-Cake\Core\Plugin::load($pluginName, ['path' => ROOT . DS, 'autoload' => true, 'routes' => $loadPluginRoutes, 'bootstrap' => $loadPluginBootstrap]);
+Cake\Core\Plugin::load('Qobo/' . $pluginName, ['path' => ROOT . DS, 'autoload' => true, 'routes' => $loadPluginRoutes, 'bootstrap' => $loadPluginBootstrap]);
