25baaadd7fcaebd6f69cec4afda8dc356d0c3518
Timur Asaliev <t.asaliev@qobo.biz>
2019-01-18 11:21:20 +0200

    Remove auth component from tests (task #9585)

--- tests/App/Controller/AppController.php
+++ tests/App/Controller/AppController.php
@@ -1,17 +1,13 @@
 <?php
 namespace Qobo\Social\Test\App\Controller;
 
-use \Cake\Controller\Controller;
+use Cake\Controller\Controller;
 
 class AppController extends Controller
 {
     public function initialize()
     {
         parent::initialize();
-        $this->loadComponent('Auth', [
-            'authenticate' => ['Form']
-        ]);
-
         $this->loadComponent('Flash');
         $this->loadComponent('RequestHandler', [
             'enableBeforeRedirect' => false,
