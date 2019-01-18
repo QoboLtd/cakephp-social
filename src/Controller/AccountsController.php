126016e97bafdfb0d029a9d88aae47ea74fbc884
Timur Asaliev <t.asaliev@qobo.biz>
2019-01-18 11:53:25 +0200

    Update accounts controller (task #9585)

--- src/Controller/AccountsController.php
+++ src/Controller/AccountsController.php
@@ -1,7 +1,7 @@
 <?php
 namespace Qobo\Social\Controller;
 
-use Qobo\Social\Controller\AppController;
+use App\Controller\AppController;
 
 /**
  * Accounts Controller
@@ -35,7 +35,7 @@ class AccountsController extends AppController
      * @return \Cake\Http\Response|void
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
-    public function view($id = null)
+    public function view(?string $id = null)
     {
         $account = $this->Accounts->get($id, [
             'contain' => ['Networks', 'Posts']
@@ -47,7 +47,7 @@ class AccountsController extends AppController
     /**
      * Add method
      *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+     * @return \Cake\Http\Response|void Redirects on successful add, renders view otherwise.
      */
     public function add()
     {
@@ -69,10 +69,10 @@ class AccountsController extends AppController
      * Edit method
      *
      * @param string|null $id Account id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+     * @return \Cake\Http\Response|void Redirects on successful edit, renders view otherwise.
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
-    public function edit($id = null)
+    public function edit(?string $id = null)
     {
         $account = $this->Accounts->get($id, [
             'contain' => []
@@ -94,10 +94,10 @@ class AccountsController extends AppController
      * Delete method
      *
      * @param string|null $id Account id.
-     * @return \Cake\Http\Response|null Redirects to index.
+     * @return \Cake\Http\Response|void Redirects to index.
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
-    public function delete($id = null)
+    public function delete(?string $id = null)
     {
         $this->request->allowMethod(['post', 'delete']);
         $account = $this->Accounts->get($id);
