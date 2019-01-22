28214aa92090df8dd59a50fe54f74c2e150cd3b7
Timur Asaliev <t.asaliev@qobo.biz>
2019-01-22 12:39:20 +0200

    Add encryption test (task #9585)

--- tests/TestCase/Model/Table/AccountsTableTest.php
+++ tests/TestCase/Model/Table/AccountsTableTest.php
@@ -1,8 +1,10 @@
 <?php
 namespace Qobo\Social\Test\TestCase\Model\Table;
 
+use Cake\Core\Configure;
 use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
+use Cake\Utility\Security;
 use Qobo\Social\Model\Entity\Account;
 use Qobo\Social\Model\Table\AccountsTable;
 use Webmozart\Assert\Assert;
@@ -103,5 +105,38 @@ class AccountsTableTest extends TestCase
         Assert::isInstanceOf($actual, Account::class);
         $this->assertSame($account, $actual);
         $this->assertFalse($actual->isDirty('credentials'));
+    }
+
+    /**
+     * Test encryption of credentials when account is ours.
+     *
+     * @return void
+     */
+    public function testEncryptCredentialsIsOurs(): void
+    {
+        $credentials = 'foobar';
+        $account = $this->Accounts->newEntity(
+            [
+                'is_ours' => true,
+                'handle' => 'test',
+                'credentials' => $credentials,
+            ],
+            [
+                'accessibleFields' => [
+                    'credentials' => true,
+                ]
+            ]
+        );
+
+        $actual = $this->Accounts->encryptCredentials($account);
+        Assert::isInstanceOf($actual, Account::class);
+        $this->assertSame($account, $actual);
+        $this->assertTrue($actual->isDirty('credentials'));
+
+        // Check decryption
+        $key = (string)Configure::read('Qobo/Social.encrypt.credentials.encryptionKey');
+        $encrypted = (string)$actual->get('credentials');
+        $decrypted = Security::decrypt($encrypted, $key);
+        $this->assertEquals($credentials, $decrypted);
     }
 }
