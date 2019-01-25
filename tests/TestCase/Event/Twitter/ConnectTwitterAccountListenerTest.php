912795b404c0349e8d66291c236d1212f4c5c988
Timur Asaliev <t.asaliev@qobo.biz>
2019-01-25 11:12:24 +0200

    Update tests and fix phpstan errors (task #9766)

--- tests/TestCase/Event/Twitter/ConnectTwitterAccountListenerTest.php
+++ tests/TestCase/Event/Twitter/ConnectTwitterAccountListenerTest.php
@@ -1,11 +1,11 @@
 <?php
 namespace Qobo\Social\Test\TestCase\Event\Twitter;
 
-use Abraham\TwitterOAuth\TwitterOAuth;
 use Cake\Event\Event;
 use Cake\Http\ServerRequest;
 use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
+use PHPUnit\Framework\MockObject\MockObject;
 use Qobo\Social\Event\EventName;
 use Qobo\Social\Event\Twitter\ConnectTwitterAccountListener;
 use RuntimeException;
@@ -22,6 +22,27 @@ class ConnectTwitterAccountListenerTest extends TestCase
      * @var string
      */
     const MOCK_URL = 'https://api.twitter.com/some/endpoint';
+
+    /**
+     * Mock OAuth token
+     *
+     * @var string
+     */
+    const OAUTH_TOKEN = 'some_oauth_token_value';
+
+    /**
+     * Mock OAuth token secret
+     *
+     * @var string
+     */
+    const OAUTH_TOKEN_SECRET = 'some_oauth_token_secret_value';
+
+    /**
+     * Mock OAuth verifier
+     *
+     * @var string
+     */
+    const OAUTH_VERIFIER = 'some_oauth_verifier';
 
     /**
      * Fixtures
@@ -67,6 +88,7 @@ class ConnectTwitterAccountListenerTest extends TestCase
      */
     public function testSetGetTwitterConnection(): void
     {
+        /** @var \PHPUnit\Framework\MockObject\MockObject&\Abraham\TwitterOAuth\TwitterOAuth $connection */
         $connection = $this->getConnectionMock();
         $this->Listener->setConnection($connection);
         $this->assertSame($connection, $this->Listener->getConnection());
@@ -105,25 +127,76 @@ class ConnectTwitterAccountListenerTest extends TestCase
     {
         $request = new ServerRequest();
         $event = $this->getEvent($request);
+        /** @var \PHPUnit\Framework\MockObject\MockObject&\Abraham\TwitterOAuth\TwitterOAuth $connection */
         $connection = $this->getConnectionMock();
         $token = [
-            'oauth_token' => 'token',
-            'oauth_token_secret' => 'secret',
+            'oauth_token' => self::OAUTH_TOKEN,
+            'oauth_token_secret' => self::OAUTH_TOKEN_SECRET,
         ];
-        $conection->expects($this->once())
+        $connection->expects($this->once())
             ->method('oauth')
             ->will($this->returnValue($token));
+        $this->Listener->setConnection($connection);
+
+        /** @var \Cake\Http\Response */
+        $response = $this->Listener->connect($event, $request);
+        $this->assertInstanceOf('Cake\Http\Response', $response);
+
+        $session = $request->getSession()->read('Twitter.connectAccount');
+        $this->assertEquals($token['oauth_token'], $session['oauthToken']);
+        $this->assertEquals($token['oauth_token_secret'], $session['oauthTokenSecret']);
+
+        $headers = $response->getHeaders();
+        $url = $headers['Location'][0] ?? null;
+        $this->assertEquals(self::MOCK_URL, $url);
+    }
+
+    /**
+     * Test get request token
+     *
+     * @return void
+     */
+    public function testGetAccessToken(): void
+    {
+        $request = new ServerRequest();
+        $event = $this->getEvent($request);
+        /** @var \PHPUnit\Framework\MockObject\MockObject&\Abraham\TwitterOAuth\TwitterOAuth $connection */
+        $connection = $this->getConnectionMock();
+        $session = [
+            'oauthToken' => self::OAUTH_TOKEN,
+            'oauthTokenSecret' => self::OAUTH_TOKEN_SECRET,
+        ];
+        $request->getSession()->write('Twitter.connectAccount', $session);
+        $request = $request->withQueryParams([
+            'oauth_token' => self::OAUTH_TOKEN,
+            'oauth_verifier' => self::OAUTH_VERIFIER,
+        ]);
+
+        $accessToken = [
+            'oauth_token' => self::OAUTH_TOKEN,
+            'oauth_token_secret' => self::OAUTH_TOKEN_SECRET,
+            'user_id' => 123456,
+            'screen_name' => 'foobar',
+        ];
+        $connection->expects($this->once())
+            ->method('oauth')
+            ->will($this->returnValue($accessToken));
 
         $this->Listener->setConnection($connection);
+
+        /** @var \Qobo\Social\Model\Entity\Account $response */
         $response = $this->Listener->connect($event, $request);
+        $this->assertInstanceOf('Qobo\Social\Model\Entity\Account', $response);
+        $this->assertTrue($response->isNew());
+        $this->assertEquals(json_encode($accessToken), $response->credentials);
     }
 
     /**
      * Returns a twitter connection mock object.
      *
-     * @return \Abraham\TwitterOAuth\TwitterOAuth
+     * @return \PHPUnit\Framework\MockObject\MockObject
      */
-    protected function getConnectionMock(): TwitterOAuth
+    protected function getConnectionMock(): MockObject
     {
         $connection = $this->getMockBuilder('Abraham\TwitterOAuth\TwitterOAuth')
             ->setConstructorArgs(['consumerKey', 'consumerSecret'])
