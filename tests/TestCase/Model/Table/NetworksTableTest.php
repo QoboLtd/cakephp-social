6fef9b72cf3d81972dc531b03f6eb9c5c30ad447
Timur Asaliev <t.asaliev@qobo.biz>
2019-01-28 12:19:27 +0200

    Update networks table tests (task #9790)

--- tests/TestCase/Model/Table/NetworksTableTest.php
+++ tests/TestCase/Model/Table/NetworksTableTest.php
@@ -1,9 +1,16 @@
 <?php
 namespace Qobo\Social\Test\TestCase\Model\Table;
 
+use InvalidArgumentException;
+
+use Cake\ORM\RulesChecker;
 use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
+use Cake\Validation\Validator;
 use Qobo\Social\Model\Table\NetworksTable;
+use Qobo\Social\Provider\ProviderRegistry;
+use Qobo\Social\Provider\ProviderInterface;
+use Qobo\Social\Test\App\Provider\TestProvider;
 
 /**
  * Qobo\Social\Model\Table\NetworksTable Test Case
@@ -71,6 +78,63 @@ class NetworksTableTest extends TestCase
      */
     public function testValidationDefault(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $validator = new Validator();
+        $result = $this->Networks->validationDefault($validator);
+        $this->assertInstanceOf(Validator::class, $result);
+    }
+
+    /**
+     * Test buildRules method
+     *
+     * @return void
+     */
+    public function testBuildRules(): void
+    {
+        $rules = new RulesChecker();
+        $result = $this->Networks->buildRules($rules);
+        $this->assertInstanceOf(RulesChecker::class, $result);
+    }
+
+    /**
+     * Test social provider getter of Network entity.
+     *
+     * @return void
+     */
+    public function testEntityGetSocialProvider(): void
+    {
+        // Add test provider to the registry.
+        $registry = ProviderRegistry::getInstance();
+        $registry->set('twitter', 'test', TestProvider::class);
+
+        /** @var \Qobo\Social\Model\Entity\Network $network */
+        $network = $this->Networks->find('all')->where(['name' => 'twitter'])->first();
+        $this->assertNotNull($network);
+        /** @var \Qobo\Social\Test\App\Provider\TestProvider $provider */
+        $provider = $network->getSocialProvider('test');
+        $this->assertInstanceOf(ProviderInterface::class, $provider);
+        $this->assertEquals($network->oauth_consumer_key, $provider->getConsumerKey());
+        $this->assertEquals($network->oauth_consumer_secret, $provider->getConsumerSecret());
+    }
+
+    /**
+     * Test social provider getter of Network entity.
+     *
+     * @return void
+     */
+    public function testEntityGetInvalidSocialProvider(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        /** @var \Qobo\Social\Model\Entity\Network $network */
+        $network = $this->Networks->find('all')->where(['name' => 'twitter'])->first();
+        $this->assertNotNull($network);
+
+        try {
+            $network->getSocialProvider('test');
+        } catch (InvalidArgumentException $e) {
+            $this->assertContains('not a valid social provider', $e->getMessage());
+
+            throw $e;
+        }
     }
 }
