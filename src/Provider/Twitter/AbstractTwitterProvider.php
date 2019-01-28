5c631b58a7f287ce5527bd8bba3458d7d6419861
Timur Asaliev <t.asaliev@qobo.biz>
2019-01-28 16:50:21 +0200

    Add abstract twitter providers (task #9801)

---
+++ src/Provider/Twitter/AbstractTwitterProvider.php
@@ -0,0 +1,59 @@
+<?php
+namespace Qobo\Social\Provider\Twitter;
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+use Qobo\Social\Provider\ProviderInterface;
+
+/**
+ * Twitter 30day search premium API
+ *
+ * @see https://developer.twitter.com/en/docs/tweets/search/quick-start/premium-30-day.html
+ */
+abstract class AbstractTwitterProvider implements ProviderInterface
+{
+    /**
+     * Consumer key.
+     * @var string
+     */
+    protected $consumerKey;
+
+    /**
+     * Consumer key secret.
+     * @var string
+     */
+    protected $consumerSecret;
+
+    /**
+     * Twitter client.
+     * @var \Abraham\TwitterOAuth\TwitterOAuth
+     */
+    protected $client;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setCredentials(string $consumerKey, string $consumerSecret): void
+    {
+        $this->consumerKey = $consumerKey;
+        $this->consumerSecret = $consumerSecret;
+    }
+
+    /**
+     * Returns an instance of the twitter client.
+     *
+     * @return \Abraham\TwitterOAuth\TwitterOAuth Twitter client.
+     */
+    public function getClient(): TwitterOAuth
+    {
+        if (!($this->client instanceof TwitterOAuth)) {
+            $this->client = new TwitterOAuth($this->consumerKey, $this->consumerSecret);
+        }
+
+        return $this->client;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    abstract public function read(array $options = []);
+}
