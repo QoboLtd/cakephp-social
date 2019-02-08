695b921eb5be13e3e30e1fe021f1849d9659c78b
Timur Asaliev <t.asaliev@qobo.biz>
2019-02-08 12:44:37 +0200

    Add test twitter publisher (task #9923)

---
+++ tests/App/Publisher/Twitter/TwitterPublisher.php
@@ -0,0 +1,26 @@
+<?php
+namespace Qobo\Social\Test\App\Publisher\Twitter;
+
+use stdClass;
+
+use Qobo\Social\Publisher\Twitter\TwitterPublisher as BaseTwitterPublisher;
+
+class TwitterPublisher extends BaseTwitterPublisher
+{
+    /**
+     * Paylaod filename to retreive.
+     * @var string
+     */
+    public $filename = 'twitter_response_publisher';
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function callApi(array $options)
+    {
+        $file = TESTS . 'data' . DS . $this->filename . '.json';
+        $data = (string)file_get_contents($file);
+
+        return json_decode($data);
+    }
+}
